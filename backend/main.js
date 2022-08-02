b01f64e8a01b2cbbecbce99be29c617f02c20eb7
Jan Haubrock <jan.haubrock@live.com>
2022-08-02 22:26:22 +0200

    add serving of build react to backend

--- backend/main.js
+++ backend/main.js
@@ -6,6 +6,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+
 const ingredientRouter = require('./router/ingredient')
 const unitRouter = require('./router/units')
 const recipeRouter  = require('./router/recipes')
@@ -22,9 +23,7 @@ const port = 1337
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
-	res.status(403).end()
-})
+app.use(express.static("build"))
 app.use('/units', unitRouter)
 app.use('/ingredients', ingredientRouter)
 app.use('/recipes',recipeRouter)
