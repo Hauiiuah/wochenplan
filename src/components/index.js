5807806b1dcf02699411c86d7e9d873f81ef6a22
Jan Haubrock <jan.haubrock@live.com>
2022-09-13 00:12:44 +0200

    add extracted components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 import MenuCards from './menu/menucards'
 import ModalFrame from "./modalframe";
-export { MenuCards, ModalFrame }
+import ShoppingList from "./shoppinglist";
+import RecipeSelector from "./recipeselector";
+import WeekNavigator from './weeknavigator'
+import Layout from './layout'
+
+export { MenuCards, ModalFrame, RecipeSelector, ShoppingList, WeekNavigator, Layout}
