2ab3b1de9a00a144e005e69e7dec41d07f475082
Jan Haubrock <jan.haubrock@live.com>
2022-09-14 01:48:39 +0200

    add search for recipeselector

--- src/components/recipeselector.js
+++ src/components/recipeselector.js
@@ -4,6 +4,7 @@ import {RecipeProvider} from "../services";
 const RecipeSelector = ({selectRecipeCallback}) => {
     const [recipes,setRecipes] = useState([]);
     const [selectedRecipe, setSelectedRecipe]= useState(null)
+    const [searchFilter,setSearchFilter]= useState("")
 
     useEffect(()=>{(async ()=>{
         const recipes = await RecipeProvider.getAll()
@@ -12,23 +13,36 @@ const RecipeSelector = ({selectRecipeCallback}) => {
 
 
     const localSelectRecipe=(recipe)=>{
-        console.log('localSelectRecipe')
         selectRecipeCallback(recipe)
         setSelectedRecipe(recipe)
     }
 
+    const recipeFilter=(recipe)=>{
+
+        return recipe.name.toLowerCase().includes(searchFilter.toLowerCase())
+    }
+
     return (
+        <>
+            <div className="searchbar">
+                <input type="text" placeholder="Suche" onChange={(e)=>setSearchFilter(e.target.value)} value={searchFilter} />
+            </div>
         <div className="selectList">
             <ul>
-                {recipes.map( (recipe) =>
+                {recipes.filter(recipeFilter).map( (recipe) =>
                     {
                         const isSelected = selectedRecipe?._id === recipe._id ? "selected" : ""
-                        return (<li key={recipe._id} className={isSelected} onClick={()=>{localSelectRecipe(recipe)}}>{recipe.name}</li>)
+                        return (
+                            <li key={recipe._id} className={isSelected} onClick={()=>{localSelectRecipe(recipe)}}>
+                                <img src={`./assets/recipes/${recipe.img}`} alt={recipe.name} className="menu-img" /> <p>{recipe.name}</p>
+                            </li>
+                        )
                     }
 
                 )}
             </ul>
         </div>
+        </>
     )
 }
 
